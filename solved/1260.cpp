374eace83fb0b6d47c306ae386ab4891494ec607
lucie-perusonari <perusonari@gmail.com>
2026-02-17 21:22:44 +0900

    solve: 1260번 DFS와 BFS 문제 풀이 업데이트

--- solved/1260.cpp
+++ solved/1260.cpp
@@ -1,117 +1,84 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <math.h>
-#include <map>
-#include <deque>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-/*
-    *문제 번호 : 1260
+int n, m, v;
+int graph[1001][1001];
+bool visited_bfs[1001];
+bool visited_dfs1[1001];
+bool visited_dfs2[1001];
 
-    *문제명 : DFS와 BFS
+stack<int> S;
+queue<int> Q;
 
-    *문제 설명 :
-          그래프를 DFS로 탐색한 결과와 BFS로 탐색한 결과를 출력하는 프로그램을 작성하시오. 단, 방문할 수 있는 정점이 여러 개인 경우에는 정점 번호가 작은 것을 먼저 방문하고, 더 이상 방문할 수 있는 점이 없는 경우 종료한다. 정점 번호는 1번부터 N번까지이다.
+void stack_dfs(int st) {
+	S.push(st);
 
-    *입력 :
-          첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000), 간선의 개수 M(1 ≤ M ≤ 10,000), 탐색을 시작할 정점의 번호 V가 주어진다. 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다. 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다. 입력으로 주어지는 간선은 양방향이다.
+	while (!S.empty()) {
+		int v = S.top(); S.pop();
 
-    *출력 :
-          첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
-*/
+		if (visited_dfs1[v] == true) continue;
+		visited_dfs1[v] = true;
 
-map<int, vector<int> > graph;
-vector<bool> is_dfs_checked(1001, false);
+		cout << v << ' ';
 
-string result = "";
+		// 스택이라 낮은 순번부터 방문하고 싶으면 역방향 순회가 필요.
+		for (int i = n;i > 0; i--) {
+			if (graph[v][i] == 0) continue;
 
-void dfs(int a) {
-      if (is_dfs_checked[a] == true) {
-            return;
-      }
-
-      result.append(to_string(a));
-      result.append(" ");
-      is_dfs_checked[a] = true;
-
-      if (graph.count(a) == false) {
-            return;
-      }
-
-      for (const auto& entry : graph[a]) {
-            dfs(entry);
-      }
+			S.push(i);
+		}
+	}
 }
 
-deque<int> bfs_queue = {};
-vector<bool> is_bfs_checked(1001, false);
+// 함수의 실행 순서에 따라서 자연스럽게 방문이 진행 됨.
+void recursive_dfs(int v) {
+	cout << v << " ";
+	visited_dfs2[v] = true;
 
-void bfs(int a) {
-      bfs_queue.push_back(a);
-      is_bfs_checked[a] = true;
+	for (int i = 1;i <= n;i++) {
+		if (visited_dfs2[i] == true || graph[v][i] == 0) continue;
 
-      while (true) {
-            if (bfs_queue.empty() == true) {
-                  break;
-            }
+		recursive_dfs(i);
+	}
+}
 
-            int b = bfs_queue.front();
-            bfs_queue.pop_front();
+void bfs(int v) {
+	Q.push(v);
+	visited_bfs[v] = true;
 
-            // cout << b << " ";
-            result.append(to_string(b));
-            result.append(" ");
+	while (!Q.empty()) {
+		int v = Q.front(); Q.pop();
 
-            for (auto entry : graph[b]) {
-                  if (is_bfs_checked[entry] == true) {
-                        continue;
-                  }
-                  is_bfs_checked[entry] = true;
-                  bfs_queue.push_back(entry);
-            }
-      }
+		cout << v << ' ';
+
+		for (int i = 1;i <= n;i++) {
+			if (visited_bfs[i] == true || graph[v][i] == 0) continue;
+
+			Q.push(i);
+			visited_bfs[i] = true;
+		}
+	}
 }
 
 int main() {
-      ios_base::sync_with_stdio(false);
-      cin.tie(NULL);
+	cin.tie(0);
+	ios::sync_with_stdio(false);
 
-      // 
-      int n, m, v;
-      cin >> n >> m >> v;
+	cin >> n >> m >> v;
 
 
-      // 간단하게 탐색만 구현해보자.
-      while (m--) {
-            int a, b;
-            cin >> a >> b;
+	for (int i = 0;i < m;i++) {
+		int a, b;
+		cin >> a >> b;
 
-            if (graph.count(a) == false) {
-                  graph[a] = {};
-            }
+		graph[a][b] = 1;
+		graph[b][a] = 1;
+	}
 
-            if (graph.count(b) == false) {
-                  graph[b] = {};
-            }
+	recursive_dfs(v);
 
-            // 양방향 연결.
-            // 중복이 생기는 것을 염두해야할 것 같음.
-            graph[a].push_back(b);
-            graph[b].push_back(a);
-      }
+	cout << "\n";
 
-      for (auto& entry : graph) {
-            sort(entry.second.begin(), entry.second.end());
-      }
-
-      dfs(v);
-      result.pop_back();
-      result += "\n";
-
-      bfs(v);
-      result.pop_back();
-      cout << result;
+	bfs(v);
 }
